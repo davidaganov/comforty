f474abd262535534aa8f6f4601b8ae0d9a49aff1
David <davidaganov21@gmail.com>
2023-03-16 15:23:38 +0500

    Updates

---
+++ landing-comforty/ProductsView-bd08a405.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o,a as t}from"./index-676d333f.js";const s={},n=t("h2",null,"Продукты",-1),r=[n];function _(a,d){return c(),o("div",null,r)}const i=e(s,[["render",_]]);export{i as default};
