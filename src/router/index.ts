41a752fb3c31ca5f6ed35fb104c7bd2bfa60f4ba
David <davidaganov21@gmail.com>
2023-03-16 17:43:23 +0500

    :wrench: Fix deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import Translation from "../i18n/translation"
-import { createRouter, createWebHistory, RouterView } from "vue-router"
+import { createRouter, createWebHashHistory, RouterView } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/:locale?",
--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: "dist",
-    assetsDir: "landing-comforty/"
-  }
+    outDir: "dist"
+  },
+  base: "/landing-comforty/"
 })
