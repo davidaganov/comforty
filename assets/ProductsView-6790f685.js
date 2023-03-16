ad40449057f1956a824bbdb480b03902f43f0fbe
David <davidaganov21@gmail.com>
2023-03-16 15:45:44 +0500

    Updates

---
+++ assets/ProductsView-6790f685.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o,a as t}from"./index-d68f0abc.js";const s={},n=t("h2",null,"Продукты",-1),r=[n];function _(a,d){return c(),o("div",null,r)}const i=e(s,[["render",_]]);export{i as default};
