ad319d609c5438a490f0e97e617473c855a211ba
David <davidaganov21@gmail.com>
2023-05-07 21:50:50 +0500

    :wrench: Added sorting on page Products

--- src/components/Base/BaseSorting.vue
+++ src/components/Base/BaseSorting.vue
@@ -2,70 +2,189 @@
   <div
     class="sorting"
     role="listbox"
+    :class="{ 'sorting--dropdown': dropdown, 'sorting--open': open }"
+    :aria-expanded="open || !dropdown"
+    @click="openDropdown()"
   >
     <span
       id="sortLabel"
-      class="visually-hidden"
+      class="sorting__label"
+      :class="!dropdown ? 'visually-hidden' : ''"
+      :tabindex="dropdown ? '0' : '-1'"
     >
-      Sorting products:
+      {{ $t("sorting.label") }}
     </span>
-    <button
-      type="button"
-      class="sorting__tag"
-      role="option"
-      :id="slug"
-      :class="{ 'sorting__tag--active': store.isSelectedSortingTag(slug) }"
-      :aria-selected="false"
-      :aria-labelledby="`sortLabel ${slug}`"
-      :key="id"
-      v-for="{ id, slug, tag } in tags"
-      @click="store.setSelectedSortingTag(slug)"
-    >
-      {{ tag[Translation.currentLocale] }}
-    </button>
+    <Transition name="sorting--dropdown">
+      <div
+        v-show="open"
+        class="sorting__container"
+      >
+        <button
+          type="button"
+          class="sorting__tag"
+          role="option"
+          :id="`tag-${slug}`"
+          :class="{ 'sorting__tag--active': store.isSelectedSortingTag(slug) }"
+          :aria-selected="store.isSelectedSortingTag(slug)"
+          :aria-labelledby="`sortLabel tag-${slug}`"
+          :key="id"
+          v-for="{ id, slug, tag } in tags"
+          @click="store.setSelectedSortingTag(slug)"
+        >
+          {{ tag[Translation.currentLocale] }}
+        </button>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from "vue"
 import { useStore } from "../../stores"
 import Translation from "../../i18n/translation"
 
+const props = defineProps<{ dropdown?: boolean }>()
+
 const store = useStore()
 const tags = store.getSortingTags
+const dropdown = ref(props.dropdown)
+const open = ref(false)
+
+const openDropdown = () => {
+  dropdown.value ? (open.value = !open.value) : null
+}
+
+onMounted(() => {
+  if (!dropdown.value) open.value = true
+})
 </script>
 
 <style scoped lang="scss">
 .sorting {
-  display: flex;
-  justify-content: center;
-  gap: 0.8rem;
-  margin-top: 1.6rem;
+  $parent: &;
+
+  &__container {
+    display: flex;
+    justify-content: center;
+  }
+
   &__tag {
-    padding: 0.8rem;
-    text-transform: uppercase;
-    font: 500 1.6rem/110% var(--main-font);
+    font: 400 1.6rem/110% var(--main-font);
     letter-spacing: 0.05em;
-    color: #9a9caa;
     cursor: pointer;
     transition: all 0.2s;
-    &--active {
-      color: var(--color-black);
-      box-shadow: inset 0px -0.2rem 0px #038053;
-      &:hover {
-        box-shadow: inset 0px -0.2rem 0px var(--color-accent);
-      }
-      &:focus-visible {
-        background-color: #038053;
-        color: var(--color-white);
+  }
+
+  &--open {
+    #{$parent}__label {
+      background-color: var(--color-accent);
+      color: var(--color-white);
+      border-color: var(--color-accent);
+    }
+  }
+
+  &--dropdown {
+    position: relative;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    height: 5rem;
+
+    &-enter-active,
+    &-leave-active {
+      transition: opacity 0.2s ease;
+    }
+
+    &-enter-from,
+    &-leave-to {
+      opacity: 0;
+    }
+
+    &:not(#{$parent}--open) {
+      #{$parent}__label {
+        color: var(--color-black);
+        border-color: #e1e3e5;
       }
     }
-    &:not(&--active) {
-      &:hover {
-        color: var(--color-black);
-      }
+
+    #{$parent}__container {
+      position: absolute;
+      flex-direction: column;
+      width: 100%;
+      left: 0;
+      top: 5.5rem;
+      border-radius: 1rem;
+      box-shadow: 0px 1.9rem 3rem 0px rgba(39, 35, 67, 0.12);
+      z-index: 3;
+    }
+
+    #{$parent}__label {
+      padding: 1.6rem 2.4rem;
+      font: 500 1.6rem/110% var(--main-font);
+      border-radius: 0.8rem;
+      border-width: 0.1rem;
+      border-style: solid;
+      transition: all 0.2s;
+      user-select: none;
+      cursor: pointer;
+      &:hover,
       &:focus-visible {
         background-color: var(--color-accent);
         color: var(--color-white);
+        border-color: var(--color-accent);
+      }
+    }
+
+    #{$parent}__tag {
+      padding: 1.2rem 1rem;
+      background-color: var(--color-white);
+      color: var(--color-black);
+      &:first-of-type {
+        border-radius: 1rem 1rem 0 0;
+      }
+      &:last-of-type {
+        border-radius: 0 0 1rem 1rem;
+      }
+      &:hover,
+      &:focus-visible {
+        background-color: var(--color-accent-hover);
+        color: var(--color-white);
+      }
+      &--active {
+        color: var(--color-white);
+        background-color: var(--color-accent);
+      }
+    }
+  }
+
+  &:not(&--dropdown) {
+    #{$parent}__container {
+      gap: 0.8rem;
+    }
+
+    #{$parent}__tag {
+      padding: 0.8rem;
+      color: var(--color-dark-gray);
+      text-transform: uppercase;
+      &:not(#{$parent}--active) {
+        &:hover {
+          color: var(--color-black);
+        }
+        &:focus-visible {
+          background-color: var(--color-accent);
+          color: var(--color-white);
+        }
+      }
+      &--active {
+        color: var(--color-black);
+        box-shadow: inset 0px -0.2rem 0px #038053;
+        &:hover {
+          box-shadow: inset 0px -0.2rem 0px var(--color-accent);
+        }
+        &:focus-visible {
+          background-color: #038053;
+          color: var(--color-white);
+        }
       }
     }
   }
--- src/components/Blocks/BlockProductsGrid.vue
+++ src/components/Blocks/BlockProductsGrid.vue
@@ -4,16 +4,14 @@
     id="products"
   >
     <BaseInner class="products__inner">
-      <BaseTitle class="products__title">
-        {{ $t("blocks.products.title") }}
-      </BaseTitle>
-
-      <BaseSorting />
-
+      <BaseSorting
+        class="products__sorting"
+        :dropdown="true"
+      />
       <div class="products__list">
         <BaseCardProduct
           class="products__item"
-          v-for="product in products.slice(0, 8)"
+          v-for="product in products"
           v-bind="product"
           :key="product.id"
         />
@@ -23,30 +21,55 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from "vue"
-import { useStore } from "../../stores"
+import { ref, watch, onMounted } from "vue"
 import { storeToRefs } from "pinia"
+import { useRoute, useRouter } from "vue-router"
+import { useStore } from "../../stores"
 
 import BaseInner from "../Base/BaseInner.vue"
-import BaseTitle from "../Base/BaseTitle.vue"
-import BaseSorting from "../Base/BaseSorting.vue"
 import BaseCardProduct from "../Base/BaseCardProduct.vue"
+import BaseSorting from "../Base/BaseSorting.vue"
+
+const route = useRoute()
+const router = useRouter()
 
 const store = useStore()
 const { getSelectedSortingTag } = storeToRefs(store)
 
-const products = ref(store.getSortingProducts())
+const products = ref()
+
+const addQueryParams = () => {
+  const selectedTag = route.query.attr ? route.query.attr : "all"
+  const queryParams = { attr: selectedTag }
+
+  if (typeof selectedTag === "string") {
+    products.value = store.getSortingProducts(selectedTag)
+    store.setSelectedSortingTag(selectedTag)
+  }
+
+  router.push({ path: route.path, query: queryParams })
+}
 
 watch(getSelectedSortingTag, () => {
   products.value = store.getSortingProducts(getSelectedSortingTag.value)
+  router.push({ path: route.path, query: { attr: getSelectedSortingTag.value } })
+})
+
+onMounted(() => {
+  addQueryParams()
 })
 </script>
 
 <style scoped lang="scss">
 .products {
-  &__title {
-    text-align: center;
-    margin-bottom: 1.6rem;
+  &__inner {
+    display: flex;
+    flex-direction: column;
+    padding-top: 3rem;
+  }
+
+  &__sorting {
+    align-self: flex-end;
   }
 
   &__list {
